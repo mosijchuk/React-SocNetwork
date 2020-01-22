d75fbb40d09ea99cd9a4de8c70d1274501ec91ac
xkosterx <vteccb@gmail.com>
2020-01-22 20:45:04 +0200

    Updates

---
+++ precache-manifest.385a06ac403ad82c771decf5188cccb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187f012589011b79cfb639db629257c9",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "c66080efc0159c1e7e0e",
+    "url": "/React/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "4bf0aaa429489d4b173f",
+    "url": "/React/static/css/3.aaac5c0a.chunk.css"
+  },
+  {
+    "revision": "e7d98ad02707fa37e214",
+    "url": "/React/static/css/4.2a9fc49a.chunk.css"
+  },
+  {
+    "revision": "f18abbbbef2254d26e96",
+    "url": "/React/static/css/main.ec0030b4.chunk.css"
+  },
+  {
+    "revision": "c66080efc0159c1e7e0e",
+    "url": "/React/static/js/2.42e19201.chunk.js"
+  },
+  {
+    "revision": "4bf0aaa429489d4b173f",
+    "url": "/React/static/js/3.64869262.chunk.js"
+  },
+  {
+    "revision": "e7d98ad02707fa37e214",
+    "url": "/React/static/js/4.57caee08.chunk.js"
+  },
+  {
+    "revision": "f18abbbbef2254d26e96",
+    "url": "/React/static/js/main.e9894011.chunk.js"
+  },
+  {
+    "revision": "670fd63d8f42624be41d",
+    "url": "/React/static/js/runtime~main.39a405e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce9ed53df332bff18b08efd0f46c0834.js"
+  "/React/precache-manifest.385a06ac403ad82c771decf5188cccb1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
